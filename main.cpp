33bdd24df1e1c373949808cba8d28c26b61b45cb
Shiku03 <nikki.shiku@gmail.com>
2024-10-23 20:54:00 +0300

    added loops in Draw function

--- main.cpp
+++ main.cpp
@@ -21,6 +21,29 @@ score=0;
 
 void Draw()
 {
+system("cls");
+for (int i=0; i< width; i++)
+cout << "#";
+cout << endl;
+
+for (int i=0; i<height; i++) 
+{
+     for (int j=0; j<width; j++) 
+     {
+        if (j == 0)
+        cout << "#";
+    else 
+    cout  << " ";
+
+if (j == width-1)
+cout << "#";
+coutt << endl;
+
+}
+
+for (int i=0; i< width; i++)
+cout << "#";
+cout << endl;
 
 }
 
